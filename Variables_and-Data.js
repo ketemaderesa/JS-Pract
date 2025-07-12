ac7e43b077f9c7b918d061eef0872b16f5a2e862
Ketema Deresa <ketemaderesa123@gmail.com>
2025-07-12 12:51:40 +0100

    Variables_and-Data.js is addded here

---
+++ Variables_and-Data.js
@@ -0,0 +1,15 @@
+// // Declaring variables
+let age =22; //  here, age is a variable that stores the number 30.
+console.log(age); // console.log() prints the value of age to the console.
+const name ='chala'; // name is a constant variable holding the text "chala".
+console.log(name);
+//   Important Keywords:
+//let – for variables that can change
+
+//const – for variables that stay constant
+// Data Types
+let isCoffeeReady =true;
+console.log(typeof isCoffeeReady);
+
+let student = { name: "Liya", age: 18 };
+console.log(student.name); // Output: Liya
