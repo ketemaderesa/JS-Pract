494933f13256897184ab3e96ec82d200ca74f183
Ketema Deresa <ketemaderesa123@gmail.com>
2025-07-12 12:58:21 +0100

    Conditionals.js is addded here

---
+++ Conditionals.js
@@ -0,0 +1,17 @@
+// if, else statements
+let age = 20;
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+// else if
+let score = 85;
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else {
+  console.log("C");
+}
