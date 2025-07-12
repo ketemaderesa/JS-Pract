45e1d5eccf1f837347ae9cb9e6bcc641dff1273c
Ketema Deresa <ketemaderesa123@gmail.com>
2025-07-12 13:10:59 +0100

    Arrays.js is addded here

--- Arrays.js
+++ Arrays.js
@@ -1,3 +1,6 @@
 let fruits = ["banana", "apple", "mango"];
 console.log(fruits); 
 console.log('this prints the array index' + ' ' +fruits[1]); // Output: apple
+
+fruits.push("orange");
+console.log(fruits); // Output: ["banana", "apple", "mango", "orange"]
