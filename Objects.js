2cc8319fd97b15b1b871cdae6f2dc2852c5ea9e9
Ketema Deresa <ketemaderesa123@gmail.com>
2025-07-12 13:17:55 +0100

    Objects.js is addded here

---
+++ Objects.js
@@ -0,0 +1,8 @@
+// Store data in key-value pairs.
+let car = { brand: "Toyota", year: 2022 };
+console.log(car.brand); // Output: Toyota
+
+console.log(car);
+
+car.color = "red";
+console.log(car); // Output: { brand: "Toyota", year: 2022, color: "red" }
